a75fad4b718606767b5dd8fa1916cb1f3c039670
riesenb <bryan.van.riesen@hva.nl>
2025-05-08 12:50:10 +0200

    services update

--- src/back-end/pam/services.py
+++ src/back-end/pam/services.py
@@ -1,3 +1,6 @@
+import asyncio
+from PAM_2101 import PAM_2101
+from PAM_2102 import PAM_2102
 base_uuid = "99DBXXXX-AC2D-11E3-A5E2-0800200C9A66"
 
 
@@ -11,21 +14,34 @@ class TimeDate:
 
 
 class ActivityData:
+    # this mode is used for making a client that receives data every few seconds from the PAM device
     def __init__(self):
-        self.base_uuid = base_uuid
-        # UUID for ActivityData is 2002
+        # UUID for ActivityData is 2101
         # Check documentation for details
-        self.uuid_extension = "2101"
+        self.base_uuid = base_uuid
+        self.uuid_extension = "2101"  # for ActivityData
         self.uuid = self.base_uuid.replace("XXXX", self.uuid_extension)
+
+        asyncio.run(self.run())
+
+    async def run(self):
+        pam = PAM_2101(self.uuid)
+        await pam.run()
 
 
 class ActivityFile:
     def __init__(self):
+        # UUID for ActivityFile is 2102
         self.base_uuid = base_uuid
-        # UUID for ActivityFile is 2003
-        # Check documentation for details
         self.uuid_extension = "2102"
         self.uuid = self.base_uuid.replace("XXXX", self.uuid_extension)
+
+        # Run the PAM_2102 script to send the command and confirm transmission
+        asyncio.run(self.run())
+
+    async def run(self):
+        pam = PAM_2102(self.uuid)
+        await pam.run()
 
 
 class ActivityDownload:

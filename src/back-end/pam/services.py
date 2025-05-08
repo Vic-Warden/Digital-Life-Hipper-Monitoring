8d38d02c076eb8e8eb1fa0e697aac4db167c8b19
riesenb <bryan.van.riesen@hva.nl>
2025-05-08 13:06:26 +0200

    merge conflict resolve

--- src/back-end/pam/services.py
+++ src/back-end/pam/services.py
@@ -1,8 +1,10 @@
 import asyncio
 from PAM_2101 import PAM_2101
 from PAM_2102 import PAM_2102
-base_uuid = "99DBXXXX-AC2D-11E3-A5E2-0800200C9A66"
+from PAM_2103 import PAM_2103
 
+# Base UUID for Hipper BLE commands
+base_uuid = "99DBXXXX-AC2D-11E3-A5E2-0800200C9A66"
 
 class TimeDate:
     def __init__(self):
@@ -12,9 +14,8 @@ class TimeDate:
         self.uuid_extension = "2001"
         self.uuid = self.base_uuid.replace("XXXX", self.uuid_extension)
 
-
 class ActivityData:
-    # this mode is used for making a client that receives data every few seconds from the PAM device
+    # This mode is used for making a client that receives data every few seconds from the PAM device
     def __init__(self):
         # UUID for ActivityData is 2101
         # Check documentation for details
@@ -27,7 +28,6 @@ class ActivityData:
     async def run(self):
         pam = PAM_2101(self.uuid)
         await pam.run()
-
 
 class ActivityFile:
     def __init__(self):
@@ -43,11 +43,17 @@ class ActivityFile:
         pam = PAM_2102(self.uuid)
         await pam.run()
 
-
 class ActivityDownload:
     def __init__(self):
-        self.base_uuid = base_uuid
-        # UUID for ActivityDownload is 2004
+        # UUID for ActivityDownload is 2103
         # Check documentation for details
+        self.base_uuid = base_uuid
         self.uuid_extension = "2103"
         self.uuid = self.base_uuid.replace("XXXX", self.uuid_extension)
+
+        # Run the PAM_2103 script to receive data
+        asyncio.run(self.run())
+
+    async def run(self):
+        pam = PAM_2103(self.uuid)
+        await pam.run()

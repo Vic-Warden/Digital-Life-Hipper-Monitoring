09f8ae1ffc7ff406ac92fc172d73d9cc095783c7
MikeSchardijn <mike.schardijn@outlook.com>
2025-05-15 12:23:40 +0200

    gathering more data

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -1,4 +1,4 @@
 from services import *
 
-ActivityDownload(filename="output/data_records_MAX.csv",
+ActivityDownload(filename="output/data_records_1H.csv",
                  filelength=get_detailed_request("LAST_1_HOUR"))

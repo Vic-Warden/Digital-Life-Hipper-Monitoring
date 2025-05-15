eff5adec4594b3e11a1181ceb693f75f57804776
MikeSchardijn <mike.schardijn@outlook.com>
2025-05-15 14:13:10 +0200

    fixed no long overlooking empty values as they are stil a part of the code

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -1,4 +1,4 @@
 from services import *
 
 ActivityDownload(filename="output/data_records_15M.csv",
-                 filelength=get_detailed_request("LAST_15_MIN"))
+                 filelength=get_detailed_request("LAST_1_HOUR"))

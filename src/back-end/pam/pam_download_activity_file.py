0b9506cff868288deae7829b23a9532153fd12e6
MikeSchardijn <mike.schardijn@outlook.com>
2025-05-16 10:44:49 +0200

    3 hours of PAM data

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -1,4 +1,4 @@
 from services import *
 
-ActivityDownload(filename="output/data_records_1H.csv",
+ActivityDownload(filename="output/data_records_3H.csv",
                  filelength=get_detailed_request("LAST_3_HOURS"))

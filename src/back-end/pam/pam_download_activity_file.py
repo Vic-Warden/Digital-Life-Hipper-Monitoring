6231362496376029c599d511376275a06050995c
Warden <95080324+WardenKaiser@users.noreply.github.com>
2025-05-20 16:02:56 +0200

    SetTime synch

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -1,5 +1,7 @@
 from services import *
 
+SetTimestamp2101(label_id=90248)
+
 ActivityDownload(filename="output/data_records_1D_90248.csv",
                  filelength=get_detailed_request("LAST_1_DAY"),
                  label_id=90248)

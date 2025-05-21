87d826012e7e18e9bc7ae2f6299acf44cb2eed4d
Indigo6415 <indigo.rother@gmail.com>
2025-05-21 11:32:57 +0200

    redirect to /home

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -4,6 +4,11 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 
+@app.route('/')
+def redirect_to_home():
+    return redirect('/home')
+
+
 @app.route('/home')
 def home():
     return render_template('home.html')

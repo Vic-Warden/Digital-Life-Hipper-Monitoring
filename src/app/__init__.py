4975e34a25e53f1d5d808be0f2d711f163c12917
Warden <95080324+WardenKaiser@users.noreply.github.com>
2025-05-21 14:05:48 +0200

    Commentary

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,32 +1,36 @@
+# Import Flask
 from flask import Flask, render_template, redirect, request
 
+# Create the app Flask
 app = Flask(__name__)
 
-# Temporary
-users = {
-            'user' : 'password'
-        }
-
+# Route for the home page
 @app.route('/')
 def redirect_to_home():
+    # Redirection to the home page by default
     return redirect('/home')
 
+# Home's route
 @app.route('/home')
 def home():
+    # Render the home.html
     return render_template('home.html')
 
-# Request the user & the password
-@app.route('/login', methods=[GET])
+# Request the user & the password with GET and POST methods
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'GET':
-        username = request.args['username']
+    if request.method == 'POST':
+        # Retrieve email and password 
+        email = request.args['email']
         password = request.args['password']
-        if username in users and users[username] == password:
-            return 'Success'
-        else:
-            return 'Error'      
-    else:      
+        
+        # Redirect to home after form submission
+        return redirect('/home')
+    else:
+              
+        # Render the 'login.html' template for GET requests
         return render_template('login.html')
 
+# Start the Flask application
 if __name__ == "__main__":
     app.run(debug=True, port=6001)

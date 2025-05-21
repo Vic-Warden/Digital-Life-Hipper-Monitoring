4ff755a89d820a1bec142bb2dea67adf62a25b89
Warden <95080324+WardenKaiser@users.noreply.github.com>
2025-05-21 12:26:37 +0200

    create an user and password ( temporary )

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, redirect
-
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
+users = {
+    'user' : 'password'
+}
 
 @app.route('/')
 def redirect_to_home():
     return redirect('/home')
 
-
 @app.route('/home')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=[GET])
 def login():
-    return render_template('login.html')
-
+    render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=6001)

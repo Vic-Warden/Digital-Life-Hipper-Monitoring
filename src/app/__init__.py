3001763d705c389a73ea91009122a718320a6d03
Warden <95080324+WardenKaiser@users.noreply.github.com>
2025-05-21 11:50:27 +0200

    redirect login

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -13,6 +13,10 @@ def redirect_to_home():
 def home():
     return render_template('home.html')
 
+@app.route('/')
+def redirect_to_home():
+    return redirect('/login')
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=6001)

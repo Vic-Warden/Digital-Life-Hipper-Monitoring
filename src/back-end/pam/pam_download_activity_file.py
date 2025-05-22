09b204d42c2f1d91063067e834ad2e5f7dfa015d
MikeSchardijn <mike.schardijn@outlook.com>
2025-05-22 12:03:35 +0200

    gathering testing data

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -1,13 +1,13 @@
 from services import *
 import asyncio
 
+#
+# TimeDate(label_id=90248)
+#
+# asyncio.run(asyncio.sleep(5))
+#
+# SetTimestamp2101(label_id=90248)
 
-TimeDate(label_id=90248)
-
-asyncio.run(asyncio.sleep(5))
-
-SetTimestamp2101(label_id=90248)
-
-ActivityDownload(filename="output/data_records_1D_90248.csv",
-                 filelength=get_detailed_request("LAST_1_DAY"),
+ActivityDownload(filename="output/data_records_LAST_14_DAYS_90248.csv",
+                 filelength=get_detailed_request("LAST_14_DAYS"),
                  label_id=90248)

a3dd830c3534bdacc83e3a4ba51743acb0e611ea
MikeSchardijn <mike.schardijn@outlook.com>
2025-05-26 13:54:13 +0200

    moving files around

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -8,15 +8,15 @@ import asyncio
 #
 # SetTimestamp2101(label_id=90248)
 
-ActivityDownload(filename="output/dump/device_5",
+ActivityDownload(filename="output/dump/device_5.csv",
                  filelength=get_detailed_request("MAX"),
                  label_id=90245)
-ActivityDownload(filename="output/dump/device_3",
+ActivityDownload(filename="output/dump/device_3.csv",
                  filelength=get_detailed_request("MAX"),
                  label_id=90243)
-ActivityDownload(filename="output/dump/device_8",
+ActivityDownload(filename="output/dump/device_8.csv",
                  filelength=get_detailed_request("MAX"),
                  label_id=90248)
-ActivityDownload(filename="output/dump/device_2",
+ActivityDownload(filename="output/dump/device_2.csv",
                  filelength=get_detailed_request("MAX"),
                  label_id=90242)

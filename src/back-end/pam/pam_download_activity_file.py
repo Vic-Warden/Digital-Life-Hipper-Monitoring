7b1e232695c36e5e82c25800dd5bf0d3604e1caa
Indigo <indigo.rother@gmail.com>
2025-05-28 09:55:41 +0200

    live data

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -1,19 +1,18 @@
 from services import *
 import asyncio
 
-#
-# TimeDate(label_id=90248)
-#
-# asyncio.run(asyncio.sleep(5))
-#
-# SetTimestamp2101(label_id=90248)
+TimeDate(label_id=90243)
+
+asyncio.run(asyncio.sleep(5))
+
+SetTimestamp2101(label_id=90243)
 
 # ActivityDownload(filename="output/body_part_experiment/chest",
 #                  filelength=get_detailed_request("LAST_1_HOUR"),
 #                  label_id=90245)
-ActivityDownload(filename="output/testw",
-                 filelength=get_detailed_request("LAST_1_HOUR"),
-                 label_id=90243)
+# ActivityDownload(filename="output/testw",
+#                  filelength=get_detailed_request("LAST_1_HOUR"),
+#                  label_id=90243)
 # ActivityDownload(filename="output/body_part_experiment/hip",
 #                  filelength=get_detailed_request("LAST_1_HOUR"),
 #                  label_id=90248)

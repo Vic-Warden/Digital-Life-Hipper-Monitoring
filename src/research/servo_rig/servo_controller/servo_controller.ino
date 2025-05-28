51e3c98eba0759559bc17dceb73a8a9561de009d
MikeSchardijn <mike.schardijn@outlook.com>
2025-05-28 13:24:13 +0200

    made the servo's move more consistently

--- src/research/servo_rig/servo_controller/servo_controller.ino
+++ src/research/servo_rig/servo_controller/servo_controller.ino
@@ -1,20 +1,13 @@
-
 #include "Wire.h"
 #include "Adafruit_PWMServoDriver.h"
 
-// called this way, it uses the default address 0x40
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-// you can also call it with a different address you want
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
-// you can also call it with a different address and I2C interface
-//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
 
-#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
-#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+#define USMIN  600
+#define USMAX  2400
+#define SERVO_FREQ 50
 
-// our servo
-uint8_t servonum = 0;
+uint8_t servonum = 15;
 
 void setup() {
   Serial.begin(9600);
@@ -22,24 +15,26 @@ void setup() {
 
   pwm.begin();
   pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  pwm.setPWMFreq(SERVO_FREQ);
 
   delay(10);
 }
 
-
-
 void loop() {
-    
-  for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
+  // Use a larger step size for faster movement
+  for (uint16_t microsec = USMIN; microsec < USMAX; microsec += 10) {
     pwm.writeMicroseconds(servonum, microsec);
+    delay(5);  // Optional: reduce delay further if needed
   }
-
-  delay(500);
-  for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
-    pwm.writeMicroseconds(servonum, microsec);
-  }
+  Serial.println("A");
 
   delay(500);
 
+  for (uint16_t microsec = USMAX; microsec > USMIN; microsec -= 10) {
+    pwm.writeMicroseconds(servonum, microsec);
+    delay(5);  // Optional: reduce delay further if needed
+  }
+  Serial.println("B");
+
+  delay(500);
 }

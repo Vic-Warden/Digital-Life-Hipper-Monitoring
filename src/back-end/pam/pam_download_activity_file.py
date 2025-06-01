fdcc8be56a9a08e1612242f4a43eee4ae0225ebe
MikeSchardijn <mike.schardijn@outlook.com>
2025-06-01 20:29:01 +0200

    data from experiment day final

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -1,21 +1,23 @@
 from services import *
 import asyncio
+#
+# TimeDate(label_id=90248)
+#
+# asyncio.run(asyncio.sleep(5))
+#
+# SetTimestamp2101(label_id=8)
+#
+# asyncio.run(asyncio.sleep(5))
 
-TimeDate(label_id=90243)
-
-asyncio.run(asyncio.sleep(5))
-
-SetTimestamp2101(label_id=90243)
-
-ActivityDownload(filename="output/dump/device_5.csv",
-                 filelength=get_detailed_request("MAX"),
-                 label_id=90245)
-ActivityDownload(filename="output/dump/device_3.csv",
-                 filelength=get_detailed_request("MAX"),
-                 label_id=90243)
-ActivityDownload(filename="output/dump/device_8.csv",
+# TimeDate(label_id=90242)
+#
+# asyncio.run(asyncio.sleep(5))
+#
+# SetTimestamp2101(label_id=2)
+#
+ActivityDownload(filename="../../research/data/device__8__3.csv",
                  filelength=get_detailed_request("MAX"),
                  label_id=90248)
-ActivityDownload(filename="output/dump/device_2.csv",
+ActivityDownload(filename="../../research/data/device__2__3.csv",
                  filelength=get_detailed_request("MAX"),
                  label_id=90242)
--- src/back-end/pam/pam_get_day_data.py
+++ src/back-end/pam/pam_get_day_data.py
@@ -1,5 +1,9 @@
 from services import DayDataDownload
 
-DayDataDownload(filename="output/dayData_90245_1",
-                days=1,
-                label_id=90245)
+DayDataDownload(filename="output/dayData_90242_3_experiment",
+                days=10,
+                label_id=90242)
+
+DayDataDownload(filename="output/dayData_90248_3_experiment",
+                days=10,
+                label_id=90248)

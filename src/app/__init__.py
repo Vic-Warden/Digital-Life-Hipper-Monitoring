f4c3527d684411f6fefee7ff87c35c5b18619bc9
Indigo <indigo.rother@gmail.com>
2025-06-02 10:52:17 +0200

    small change

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -96,7 +96,7 @@ def logout():
 # Profile' route with GET & POST
 
 
-@app.route('/profile', methods=['GET', 'POST'])
+@app.route('/settings', methods=['GET', 'POST'])
 def settings():
     cookie = request.cookies.get('auth_cookie')
     valid, user_data = db.verify_cookie(cookie)

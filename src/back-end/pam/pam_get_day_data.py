1cdf540b07fee7b75c7523fcb56c9b997d5c0d7c
MikeSchardijn <mike.schardijn@outlook.com>
2025-06-04 11:32:06 +0200

    new more detailed data for reference

--- src/back-end/pam/pam_get_day_data.py
+++ src/back-end/pam/pam_get_day_data.py
@@ -1,9 +1,9 @@
 from services import DayDataDownload
 
-DayDataDownload(filename="output/dayData_90242_3_experiment",
+DayDataDownload(filename="output/_04_06_2025__dayData_90242_experiment",
                 days=10,
                 label_id=90242)
 
-DayDataDownload(filename="output/dayData_90248_3_experiment",
+DayDataDownload(filename="output/_04_06_2025__dayData_90248_experiment",
                 days=10,
                 label_id=90248)

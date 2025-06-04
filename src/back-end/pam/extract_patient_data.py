23623e91a6a23e47113dbeb94695f64e00b46be3
Warden <95080324+WardenKaiser@users.noreply.github.com>
2025-06-05 00:10:21 +0200

    Fix the issues

--- src/back-end/pam/extract_patient_data.py
+++ src/back-end/pam/extract_patient_data.py
@@ -1,8 +1,9 @@
 import mysql.connector
 
-# Connexion to the database
+# Connection to the database
 connection = mysql.connector.connect(
     host="localhost",
+    port=3306,
     user="root",
     password="superstronkrootpw",
     database="hipperdb"
@@ -12,8 +13,8 @@ cursor = connection.cursor()
 
 # Test
 patient_id = 1
-start_date = "2025-06-01"
-end_date = "2025-06-30"
+start_date = "2025-05-19"
+end_date = "2025-05-25"
 
 query = f"""
     SELECT 

2fd2223cb4fe01481c8eb5b8375578991ea55eca
Warden <95080324+WardenKaiser@users.noreply.github.com>
2025-06-05 12:34:27 +0200

    Working now

--- src/back-end/pam/extract_patient_data.py
+++ src/back-end/pam/extract_patient_data.py
@@ -1,5 +1,7 @@
 import mysql.connector
 
+import csv
+
 # Connection to the database
 connection = mysql.connector.connect(
     host="localhost",
@@ -11,11 +13,12 @@ connection = mysql.connector.connect(
 
 cursor = connection.cursor()
 
-# Test
+# Test the extraction from the data base with some start and end date 
 patient_id = 1
 start_date = "2025-06-01"
 end_date = "2025-06-10"
 
+# The Data I want to extract from the database
 query = f"""
     SELECT 
         DATE(timestamp) AS date,
@@ -28,8 +31,18 @@ query = f"""
       AND DATE(timestamp) BETWEEN '{start_date}' AND '{end_date}'
     ORDER BY date ASC;
 """
-
+# My executable
 cursor.execute(query)
+
+results = cursor.fetchall()
+
+with open('results.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Date', 'Steps', 'PAM Score', 'Zone'])
+    for row in results:
+        writer.writerow(row)
+
+
 
 for row in cursor.fetchall():
     print(row)

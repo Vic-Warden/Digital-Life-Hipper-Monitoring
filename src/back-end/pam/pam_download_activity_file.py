63afb7e785d380e2eaaf208bee52839d86239621
MikeSchardijn <mike.schardijn@outlook.com>
2025-06-11 11:25:07 +0200

    retrained the model

--- src/back-end/pam/pam_download_activity_file.py
+++ src/back-end/pam/pam_download_activity_file.py
@@ -15,22 +15,27 @@ import asyncio
 #
 # SetTimestamp2101(label_id=2)
 #
-downloader = ActivityDownload(filename="../AI/training_data/06_06_2025_device_8.csv",
-                              filelength=get_detailed_request("MAX"),
-                              label_id=90248)
-asyncio.run(downloader.run())
+# downloader = ActivityDownload(filename="../AI/training_data/06_06_2025_device_8.csv",
+#                               filelength=get_detailed_request("MAX"),
+#                               label_id=90248)
+# asyncio.run(downloader.run())
+#
+# downloader = ActivityDownload(filename="../AI/training_data/06_06_2025_device_5.csv",
+#                               filelength=get_detailed_request("MAX"),
+#                               label_id=90245)
+# asyncio.run(downloader.run())
+#
+# downloader = ActivityDownload(filename="../AI/training_data/06_06_2025_device_3.csv",
+#                               filelength=get_detailed_request("MAX"),
+#                               label_id=90243)
+# asyncio.run(downloader.run())
 
-downloader = ActivityDownload(filename="../AI/training_data/06_06_2025_device_5.csv",
-                              filelength=get_detailed_request("MAX"),
-                              label_id=90245)
-asyncio.run(downloader.run())
-
-downloader = ActivityDownload(filename="../AI/training_data/06_06_2025_device_3.csv",
-                              filelength=get_detailed_request("MAX"),
-                              label_id=90243)
-asyncio.run(downloader.run())
-
-downloader = ActivityDownload(filename="../AI/training_data/06_06_2025_device_2.csv",
+downloader = ActivityDownload(filename="../AI/training_data/11_06_2025_device_2.csv",
                               filelength=get_detailed_request("MAX"),
                               label_id=90242)
+asyncio.run(downloader.run())
+
+downloader = ActivityDownload(filename="../AI/training_data/11_06_2025_device_3.csv",
+                              filelength=get_detailed_request("MAX"),
+                              label_id=90243)
 asyncio.run(downloader.run())

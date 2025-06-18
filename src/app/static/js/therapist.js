147612eb9dfda1e95631e05a0fd8c2d85b2a794d
Darian Kardjo <darian.kardjo@hva.nl>
2025-06-18 19:39:23 +0200

    fixed the js for the therapist graph.

--- src/app/static/js/therapist.js
+++ src/app/static/js/therapist.js
@@ -3,7 +3,7 @@ const chartData = {
   dates: ['2025-05-19', '2025-05-20', '2025-05-21', '2025-05-22', '2025-05-23', '2025-05-24', '2025-05-25'],
   steps: [1000, 3000, 5000, 5800, 7200, 4000, 2000],
   pamScores: [0.25, 1.0, 1.5, 2.0, 2.5, 1.75, 0.5],
-  inactiveDays: [0, 1, 4, 6] // indices of inactive days (red background)
+  inactiveDays: [0, 1, 6] // indices of inactive days (red background)
 };
 
 // Inactive period management
